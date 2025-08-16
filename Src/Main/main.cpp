6c8bd359f0cb1184f13a96a4cce7d19a5717c604
haruoogawa <haru.spring.yodai@gmail.com>
2025-08-17 08:01:31 +0900

    ReleaseGLFWを追加

--- Src/Main/main.cpp
+++ Src/Main/main.cpp
@@ -4,8 +4,10 @@
 int main()
 {
 	app::SAppSettings Settings = {};
+#ifdef _DEBUG
 	Settings.FullScreen = false;
 	Settings.ShowFPS = true;
+#endif // _DEBUG
 
 	if (!app::CAppRunner::Run(std::make_shared<app::CLive1135App>(), Settings)) return 1;
 
